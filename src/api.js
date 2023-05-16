a57ea40db3e387a048c17750e7616b7c9f875cd0
Marcos Lima <socram.oluap2003@gmail.com>
2023-05-15 21:39:39 -0300

    .

--- src/api.js
+++ src/api.js
@@ -1,16 +1,16 @@
 import axios from 'axios';
 
+const baseURL = 'https://kanban-backend-production-d896.up.railway.app/';
+// const baseURL = 'http://localhost:3000';
+
 const api = axios.create({
-  baseURL: 'http://localhost:3000',
+  baseURL,
 });
 
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem('token');
 
-  if (
-    config.url === '/login'
-    || config.url === '/register'
-  ) {
+  if (config.url === '/login' || config.url === '/register') {
     return config;
   }
   // eslint-disable-next-line no-param-reassign
